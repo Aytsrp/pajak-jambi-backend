47b1371be5e80d024648e809392ba583b00cf143
rayhannurcholis <rayhan.a.nurcholis@student.uns.ac.id>
2026-09-08 13:34:36 +0700

    fix: correct syntax in middleware alias definition and route closure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'has.tax.object' => EnsureHasTaxObject::class,
+            'has.tax.object' => EnsureHasTaxObject::class
+            ,
         ]);
 
         $middleware->api(prepend: [
--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
     Route::post('/notifications/{id}/read', [NotificationController::class, 'markAsRead']);
     Route::post('/notifications/read-all', [NotificationController::class, 'markAllAsRead']);
-});
+});  
 
 Route::middleware(['auth:sanctum', 'has.tax.object'])->group(function () {
     Route::post('/transactions/initiate', [TransactionController::class, 'initiate']);
